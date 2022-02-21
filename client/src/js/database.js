9352754d2870af3cdd5861b4d64588a98619d711
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-20 19:51:53 -0600

    entered text into webpac.config.js

---
+++ client/src/js/database.js
@@ -0,0 +1,36 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error('putDb not implemented');
+const contactDb = await openDB('contact', 1);
+    const tx = contactDb.transaction('contact', 'readwrite');
+    const store = tx.objectStore('contact');
+    const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email })
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+    };
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+const contactDb = await openDB('contact', 1);
+    const tx = contactDb.transaction('contact', 'readwrite');
+    const store = tx.objectStore('contact');
+    const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email })
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+    };
+
+
+initdb();
