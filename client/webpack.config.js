2b08116269fdeda7c448dd56adb6be96a7ca3ed5
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-21 20:03:01 -0600

    made minor changest to webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,18 +3,35 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+const {GenerateSW} = require('workbox-webpack-plugin')
+
+
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-const {GenerateSW} = require('workbox-webpack-plugin');
-
-module.exports = {
-  plugins: [
-  new GenerateSW()
-  ]
-};
+;
+// add service worker
+module.exports = () => {
+    return {
+    mode: 'development',
+    // Entry point for filesplugins: 
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
+    },
+    // Output for our bundles
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      
+      new GenerateSW()
+  
+}
 
 // TODO: and manifest file.
-const {InjectManifest} = require('workbox-webpack-plugin');
+
 
 module.exports = {
   plugins: [
