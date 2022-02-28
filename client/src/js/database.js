2a77b7b76716f84d42bedccc03e182564a9b08ac
Michael Rawlings <rawlingsm99@gmail.com>
2022-02-27 19:42:32 -0600

    saving final changes and screen shots

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,25 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-const jateDb = await openDB('jate', 1);
-    const tx = jateDb.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email })
-    const result = await request;
-    console.log('🚀 - data saved to the database', result);
-    
+export const putDb = async (content) => {
+  console.log("PUT to the database");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction(["jate"], "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, content });
+  const result = await request;
+  console.log("🚀 - data saved to the database", result);
+}
+export const getDb = async (id, content) => {
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-const jateDb = await openDB('jate', 1);
-    const tx = jateDb.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    const request = store.add({ name: name, home_phone: home, cell_phone: cell, email: email })
-    const result = await request;
-    console.log('🚀 - data saved to the database', result);
-   
-
+  console.log('GET all from the database'); // from 23 activity, Delte from local to see if its gone after implementingf START HEREEEEE
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request[0];
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,98 +1,64 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-const {GenerateSW} = require('workbox-webpack-plugin')
-
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-;
-// add service worker
-module.exports = () => {
-    return {
-    mode: 'development',
-    // Entry point for filesplugins: 
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      cards: './src/js/cards.js'
-    },
-    // Output for our bundles
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-      new GenerateSW(),
-      
-      new InjectManifest({
-      swSrc: './src/sw.js',
-    })
 
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-  };
-
-  ]
-}
-
-// TODO: and manifest file.
-
-
-
-  
-    
- 
-
-// TODO: Add CSS loaders and babel to webpack.
-
-
-  m
-
-  
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devtool: "source-map",
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'jate List'
+      }),
+      new WorkboxPlugin.GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'JATE - editoe',
+        short_name: 'JATE',
+        description: 'Store your info',
+        background_color: '#00008B ',
+        theme_color: '#00008B ',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
-    
+
     module: {
       rules: [
         {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
-          loader: 'babel-loader',
-          options: {
-          presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
-  
-    module: {
-      rules: [
-        
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
-  };
-};
+  }
+}
